1446f10f04cd5daec6d0483e577c144cd1e8afc6
Xeiv360 <145395703+XeivUPC@users.noreply.github.com>
2025-10-20 01:27:06 +0200

    Asset Explorer Start

--- LoopieEditor/src/Interfaces/Editor/AssetsExplorerInterface.cpp
+++ LoopieEditor/src/Interfaces/Editor/AssetsExplorerInterface.cpp
@@ -1,5 +1,7 @@
 #include "AssetsExplorerInterface.h"
 
+#include "Loopie/Core/Application.h"
+
 #include <imgui.h>
 
 namespace Loopie {
@@ -9,6 +11,17 @@ namespace Loopie {
 
 	void AssetsExplorerInterface::Render() {
 		if (ImGui::Begin("Assets Explorer")) {
+			const Project& project = Application::GetInstance().m_activeProject;
+			if (project.IsEmpty()) {
+				ImGui::End();
+				return;
+			}
+			const std::filesystem::path assetPath = project.GetAssetsPath();
+
+			if (m_currentDirectory.empty())
+				m_currentDirectory = assetPath;
+			
+
 
 		}
 		ImGui::End();
--- LoopieEditor/src/Interfaces/Editor/AssetsExplorerInterface.h
+++ LoopieEditor/src/Interfaces/Editor/AssetsExplorerInterface.h
@@ -2,11 +2,15 @@
 
 #include "src/Interfaces/Interface.h"
 
+#include <filesystem>
+
 namespace Loopie {
 	class AssetsExplorerInterface : public Interface {
 	public:
 		AssetsExplorerInterface();
 		~AssetsExplorerInterface() = default;
 		void Render() override;
+	private:
+		std::filesystem::path m_currentDirectory;
 	};
 }
