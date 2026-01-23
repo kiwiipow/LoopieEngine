51c57c57fc8776fa4cc7774117362aa69aba83c6
kiwiipow <paula2005nemo@gmail.com>
2026-01-23 20:26:07 +0100

    small fix uncomented struct

--- Loopie/src/Loopie/ParticleSystemEn/ParticleComponent.h
+++ Loopie/src/Loopie/ParticleSystemEn/ParticleComponent.h
@@ -8,16 +8,16 @@ namespace Loopie
 	
 	class ParticleSystem;
 
-	//struct Particle
-	//{
-	//	vec3 position;
-	//	quaternion worldRotation;
-	//	float age;
-	//	float lifetime;
-	//	float velocity;
-	//	int spriteIndex;
+	struct Particle
+	{
+		vec3 position;
+		quaternion worldRotation;
+		float age;
+		float lifetime;
+		float velocity;
+		int spriteIndex;
 
-	//};
+	};
 	class ParticleComponent : public Component
 	{
 	private:
